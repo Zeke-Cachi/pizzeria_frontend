fe591cfe2553c118153b08e10d7fe19af867c837
Zeke-Cachi <ezequielcachi@gmail.com>
2024-02-13 12:29:47 -0300

    stores: added stores section

--- src/components/Stores.tsx
+++ src/components/Stores.tsx
@@ -1,19 +1,42 @@
 import {
   AllStores,
   Store,
+  StoreImg,
+  StoreInfo,
   StoresContainer,
 } from "../styledComponents/storesStyles";
 import { SecondaryTitle } from "../styledComponents/utils";
+import storeImg from "../assets/pizza-store-1.jpg";
 
 const Stores = () => {
   return (
     <StoresContainer>
-      <SecondaryTitle fontcolor="white" top="0rem" size="2.5rem">
+      <SecondaryTitle fontcolor="black" top="0rem" size="3rem">
         Our Stores
       </SecondaryTitle>
       <AllStores>
-        <Store></Store>
-        <Store></Store>
+        <Store>
+          <StoreImg image={storeImg}></StoreImg>
+          <StoreInfo>
+            <SecondaryTitle fontcolor="black" top="0rem" size="1.5rem">
+              123 Main Street, Philadelphia, PA 19103
+            </SecondaryTitle>
+            <SecondaryTitle fontcolor="black" top="0rem" size="1.2rem">
+              Phone number: (215) 555-1234
+            </SecondaryTitle>
+          </StoreInfo>
+        </Store>
+        <Store>
+          <StoreImg image={storeImg}></StoreImg>
+          <StoreInfo>
+            <SecondaryTitle fontcolor="black" top="0rem" size="1.5rem">
+              123 Main Street, Philadelphia, PA 19103
+            </SecondaryTitle>
+            <SecondaryTitle fontcolor="black" top="0rem" size="1.2rem">
+              Phone number: (215) 555-1234
+            </SecondaryTitle>
+          </StoreInfo>
+        </Store>
       </AllStores>
     </StoresContainer>
   );
--- src/styledComponents/storesStyles.ts
+++ src/styledComponents/storesStyles.ts
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { Image } from "../Interfaces";
 
 export const StoresContainer = styled.section`
-  height: 30rem;
+  height: 70vh;
   width: 100%;
-  background-color: black;
+  background-color: white;
   text-align: center;
   padding: 2rem;
 `;
@@ -15,8 +15,31 @@ export const AllStores = styled.div`
   justify-content: center;
 `;
 
-export const Store = styled.div``;
+export const Store = styled.div`
+  height: 40vh;
+  width: 40vw;
+  border-radius: 15px;
+  overflow: hidden;
+  margin-top: 5rem;
+  box-shadow: 0px 0px 15px 10px lightgray;
+`;
 
 export const StoreImg = styled.div<Image>`
   background-image: ${({ image }) => `url(${image})`};
+  background-size: cover;
+  background-position: center;
+  height: 50%;
+  width: 100%;
+`;
+
+export const StoreInfo = styled.div`
+  background-color: white;
+  height: 50%;
+  width: 100%;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
 `;
